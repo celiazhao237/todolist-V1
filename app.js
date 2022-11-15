4773de5eeeea7dbe910bc6cc3c6f4908123f0202
celiazhao237 <109601064+celiazhao237@users.noreply.github.com>
2022-11-15 02:59:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const date= require(__dirname+'/date.js')
 // console.log(date())
 
 const app = express()
-const items = ["Buy Food", "Cook Food", "Eat Food"]
+const items = ['Buy Food', 'Cook Food', 'Eat Food']
 const workItems =[]
 
 app.set('view engine', 'ejs')
@@ -12,65 +12,9 @@ app.use(bodyParse.urlencoded({extended:true}))
 app.use(express.static('public'))
 
 app.get('/',function(req,res){
-
   let day = date.getDate()
-  // let today = new Date() 
-  // var currentDay = today.getDay()
-  // var day=''
-  // if(currentDay===6||currentDay===0){
-  //   day='weekend'
-  //   // res.render('list', {kindOfDay: day})
-  // } else{
-  //   day='weekday'
-  //   // res.render('list', {kindOfDay: day})
-  // } 
-  // res.render('list', {kindOfDay: day})
-
-  // switch (currentDay) {
-  //   case 0:
-  //     day='Sunday';
-  //     break;
-  //   case 1:
-  //       day='Monday';
-  //     break;
-  //   case 2:
-  //       day='Tuesday';
-  //     break;
-  //   case 3:
-  //       day='Wednesday';
-  //     break;
-  //   case 4:
-  //       day='Thursday';
-  //     break;
-  //   case 5:
-  //       day='Friday';
-  //     break;
-  //   case 6:
-  //       day='Saturday';
-  //     break;
-
-  //   default: 
-  //   console.log('Error: current day is equal to: '+ currentDay);
-  // }
-  // console.log(day);
-  // res.render('list', {kindOfDay: day})
-
-  // let options = {
-  //   weekday: 'long',
-  //   day: 'numeric',
-  //   month: 'long'
-  // }
-  // let day = today.toLocaleDateString('en-US',options)
   res.render('list', {listTitle: day, newListItems: items})
 })
-
-// app.post('/',function(req,res){
-//   let item = req.body.newItem
-//   //console.log(item)
-//   // res.render('list', {newListItems: item})
-//   items.push(item)
-//   res.redirect('/')
-// })
 
 app.post('/',function(req,res){
   
